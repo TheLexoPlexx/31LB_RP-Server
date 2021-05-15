f90bb5d23f26afb0a004fa5a5d53743bb599be87
TheLexoPlexx <thelexoplexxyt@gmail.com>
2021-05-15 14:13:38 +0200

    Added clothSorter

--- src/31lb_core/server/handlers/clothHandler.ts
+++ src/31lb_core/server/handlers/clothHandler.ts
@@ -1,6 +1,6 @@
 /// <reference types="@altv/types-server" />
 import * as alt from 'alt-server';
-import { loadFileJSON } from '../fileManager';
+import { loadFileJSON, saveFileJSON } from '../fileManager';
 import * as playerManager from "../playerManager";
 //import { clothesFile, whitelistClothesFile } from '../startup';
 
@@ -16,12 +16,217 @@ export function clothSelect(player) {
 }
 */
 
-export function sortClothes() {
-  var clothFile = loadFileJSON("pedComponentVariations");
+interface PedComponentVariationDLC {
+  DlcName: string,
+  PedName: string,
+  ComponentVariations: PedComponentVariation[]
+  Props: PedProps[]
 }
 
-/*
+interface PedComponentVariation {
+  NameHash: string;
+  ComponentType: string;
+  ComponentId: number;
+  DrawableId: number;
+  TextureId: number;
+  TranslatedLabel: string;
+  Price: number;
+  RestrictionTags: string[];
+}
 
-F/M -> componentId -> drawableId -> Hash/textureid/label/price/invspacex-y/RestrictionTags/ComponentType
+interface PedProps {
+  NameHash: string,
+  AnchorPoint: string,
+  ComponentId: number,
+  DrawableId: number,
+  TextureId: number,
+  TranslatedLabel: string;
+  Price: number,
+  RestrictionTags: string[]
+}
 
-*/
+interface ClothData {
+  cHash: string;
+  price: number;
+  drawable: number;
+  inventory: InventorySpace;
+  texture: string[];
+  restrictionTags: string[];
+  componentType: string;
+}
+
+interface PropData {
+  cHash: string;
+  price: number;
+  drawable: number;
+  texture: string[];
+  restrictionTags: string[];
+  componentType: string;
+}
+
+interface InventorySpace {
+  x: number;
+  y: number;
+}
+
+const CompIds: {
+  [key: number]: string } = {
+  0: "Head",
+  1: "Masks",
+  2: "Hair Styles",
+  3: "Torsos",
+  4: "Legs",
+  5: "Bags and Parachutes",
+  6: "Shoes",
+  7: "Accessories",
+  8: "Undershirts",
+  9: "Body Armors",
+  10: "Decals",
+  11: "Tops",
+};
+
+const propIds: {
+  [key: number]: string } = {
+  0: "Hats",
+  1: "Glasses",
+  2: "Ears",
+  6: "Watches",
+  7: "Bracelets",
+};
+
+const filePath: string = "pedComponentVariations";
+let customClothCache = {
+  m: undefined,
+  f: undefined
+};
+
+const t_sort = {
+  clothes: [],
+  props: []
+}
+
+export function sortClothes() {
+  let clothFile: PedComponentVariationDLC[] = loadFileJSON("pedComponentVariations");
+
+  let tempCloth = {
+    cHash: null,
+    price: 0,
+    drawable: -1,
+    inventory: {
+      x: 10,
+      y: 12
+    },
+    texture: [],
+    restrictionTags: null,
+    componentType: null
+  };
+
+  let tempProp = {
+    cHash: null,
+    price: 0,
+    drawable: -1,
+    texture: [],
+    restrictionTags: null,
+    componentType: null
+  };
+  let tempCompId: number;
+
+  //TODO: Refactor, m&f, comp&props
+  clothFile.forEach(dlc => {
+    if (dlc.PedName == "mp_m_freemode_01") {
+
+
+    } else if (dlc.PedName == "mp_f_freemode_01") {
+
+
+    } else {
+      alt.logWarning("Ped not handled: " + dlc.DlcName + " / " + dlc.PedName);
+    }
+
+    dlc.ComponentVariations.forEach(comp => {
+      let currentHash = comp.NameHash.split("_");
+      currentHash.pop();
+      let tempHash = currentHash.join("_");
+
+      if (tempCloth.cHash == null) {
+        tempCloth.cHash = tempHash;
+        tempCompId = comp.TextureId;
+      }
+
+      if (tempCloth.cHash == tempHash) {
+        tempCloth.texture[comp.TextureId] = (comp.TranslatedLabel == (undefined || null)) ? "no_translation" : JSON.parse(JSON.stringify(comp.TranslatedLabel)).German;
+      } else {
+        if (t_sort.clothes[tempCompId] == undefined) {
+          t_sort.clothes[tempCompId] = [];
+        }
+        tempCloth.drawable = comp.DrawableId;
+        tempCloth.restrictionTags = comp.RestrictionTags;
+        tempCloth.componentType = comp.ComponentType;
+        t_sort.clothes[tempCompId].push(tempCloth);
+
+        tempCompId = comp.ComponentId;
+        tempCloth = {
+          cHash: tempHash,
+          price: 0,
+          drawable: -1,
+          inventory: {
+            x: 10,
+            y: 12
+          },
+          texture: [],
+          restrictionTags: null,
+          componentType: null
+        };
+      }
+
+      if (dlc.PedName == "mp_m_freemode_01") {
+        customClothCache.m = t_sort;
+      } else if (dlc.PedName == "mp_f_freemode_01") {
+        customClothCache.f = t_sort;
+      } else {
+        alt.logWarning("Ped not handled: " + dlc.DlcName + " / " + dlc.PedName);
+      }
+    });
+    dlc.Props.forEach(prop => {
+      let currentHash = prop.NameHash.split("_");
+      currentHash.pop();
+      let tempHash = currentHash.join("_");
+
+      if (tempProp.cHash == null) {
+        tempProp.cHash = tempHash;
+        tempCompId = prop.ComponentId;
+      }
+
+      if (tempProp.cHash == tempHash) {
+        tempProp.texture[prop.TextureId] = (prop.TranslatedLabel == (undefined || null)) ? "no_translation" : JSON.parse(JSON.stringify(prop.TranslatedLabel)).German;
+      } else {
+        if (t_sort.props[tempCompId] == undefined) {
+          t_sort.props[tempCompId] = [];
+        }
+        tempProp.drawable = prop.DrawableId;
+        tempProp.restrictionTags = prop.RestrictionTags;
+        t_sort.props[tempCompId].push(tempProp);
+
+        tempCompId = prop.ComponentId;
+        tempProp = {
+          cHash: tempHash,
+          price: 0,
+          drawable: -1,
+          texture: [],
+          restrictionTags: null,
+          componentType: null
+        };
+      }
+
+      if (dlc.PedName == "mp_m_freemode_01") {
+        customClothCache.m = t_sort;
+      } else if (dlc.PedName == "mp_f_freemode_01") {
+        customClothCache.f = t_sort;
+      } else {
+        alt.logWarning("Ped not handled: " + dlc.DlcName + " / " + dlc.PedName);
+      }
+    });
+  });
+  
+  saveFileJSON("_clothTest", customClothCache);
+}
