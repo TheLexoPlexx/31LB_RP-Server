3117a867d3336aae6e5a6dd9dcf53a6700f48506
Alex <thelexoplexxyt@gmail.com>
2021-05-19 21:03:26 +0200

    Added github-todo and vscodeignore and rebuilt

---
+++ src/31lb_core/server/managers/factionManager.ts
@@ -0,0 +1,8 @@
+/// <reference types="@altv/types-server" />
+import * as alt from 'alt-server';
+import { database } from './../startup';
+
+interface Faction {
+  displayname: string;
+  description: 
+}
